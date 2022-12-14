45583e489b9bd0a3c7f41a85dbd3b227f051531d
Vlad Makhnenko <vlmakh@gmail.com>
2022-12-14 07:04:41 +0200

    update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,13 +1,4 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-const MAIN_URL = 'https://api.themoviedb.org/3';
-const API_KEY = '7944ae355bdc42ac579681e106149d6b';
-
-const fetchTrends = async () => {
-  const response = await axios.get(
-    `${MAIN_URL}/trending/all/day?api_key=${API_KEY}&language=en-US&page=1`
-  );
-  return response.data;
-};
-
-export { fetchTrends };
+// const MAIN_URL = 'https://api.themoviedb.org/3';
+// const API_KEY = '7944ae355bdc42ac579681e106149d6b';
