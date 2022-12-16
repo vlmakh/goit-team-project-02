262be5c2a71c0366ba4ecd787a4ddcf9cccda125
Galina <galkozavrik@ukr.net>
2022-12-16 18:57:03 +0300

    реалізує поставлену задачу
    потребує правки, бо вручну задаються вотчид та черга.

--- src/js/render-from-storage.js
+++ src/js/render-from-storage.js
@@ -1,6 +1,9 @@
 import { createGalleryMarkup } from './create-gallery-markup';
 import refs from './refs';
 import { watched, queue } from './local-storage';
+import { getArrayofMovies } from './api';
+
+// console.log(getArrayofMovies(['12', '21']).then(data => data.resaults));
 
 // Mykola's buttons
 const watchedRef = document.querySelector('[data-id="watched-btn"]');
@@ -9,17 +12,23 @@ const queueRef = document.querySelector('[data-id="queue-btn"]');
 watchedRef.addEventListener('click', showWatched);
 queueRef.addEventListener('click', showQueue);
 
-console.log(watchedRef);
-console.log(queueRef);
-console.log(createGalleryMarkup);
-console.log(refs);
-
+watched.push(112, 122);
+queue.push(112, 122, 27, 45);
 function showWatched() {
   console.log('clik on watched');
-  // refs.library.innerHTML = createGalleryMarkup(watched);
+  getArrayofMovies(watched).then(data => {
+    console.log(data);
+
+    refs.library.insertAdjacentHTML('beforeend', createGalleryMarkup(data));
+  });
 }
 
 function showQueue() {
   console.log('clik on queue');
+  getArrayofMovies(queue).then(data => {
+    console.log(data);
+
+    refs.library.insertAdjacentHTML('beforeend', createGalleryMarkup(data));
+  });
   //   refs.library.innerHTML = createGalleryMarkup(queue);
 }
