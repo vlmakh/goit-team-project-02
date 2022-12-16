9ad09d950b2561f5b2f9bdb7acf311c11664dc77
vlmakh <106901780+vlmakh@users.noreply.github.com>
2022-12-16 18:20:22 +0000

    Deploying to gh-pages from @ vlmakh/goit-team-project-02@8fdc1d9908f654b56a571ed0e0a32a968b5c09a3 🚀

--- library.4c4c2cc5.js
+++
@@ -1,2 +0,0 @@
-!function(){var e="watched",a="queue";JSON.parse(localStorage.getItem(e)),JSON.parse(localStorage.getItem(a))}();
-//# sourceMappingURL=library.4c4c2cc5.js.map
--- library.b7e5eed5.js
+++
@@ -1,2 +0,0 @@
-JSON.parse(localStorage.getItem("watched")),JSON.parse(localStorage.getItem("queue"));
-//# sourceMappingURL=library.b7e5eed5.js.map
