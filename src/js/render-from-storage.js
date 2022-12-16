b96f43a02d8fb4cad3953283d370d21120196886
Galina <galkozavrik@ukr.net>
2022-12-16 18:11:54 +0300

    видаляє зайвий код

--- src/js/render-from-storage.js
+++ src/js/render-from-storage.js
@@ -1,27 +1,6 @@
-// функції додавання  id філтму у выдповыдний масив
-
-// Denis's buttons
-// const addWatchedRef = document.querySelector('[addToWatched]');
-// const addQueueRef = document.querySelector('[addToQueue]');
-
-// addWatchedRef.addEventListener('click', onAddToWatched);
-// addQueueRef.addEventListener('click', onAddToQueue);
-
-// function onAddToWatched(id) {
-//   if (watched.includes(id)) addWatchedRef.setAttribute('disable', true);
-//   else watched.push(id);
-// }
-
-// function onAddToQueue(id) {
-//   if (queue.includes(id)) addQueueRef.setAttribute('disable', true);
-//   else queue.push(id);
-// }
-
-//***************
 import { createGalleryMarkup } from './create-gallery-markup';
 import refs from './refs';
 import { watched, queue } from './local-storage';
-
 
 // Mykola's buttons
 const watchedRef = document.querySelector('[data-id="watched-btn"]');
@@ -37,10 +16,10 @@ console.log(refs);
 
 function showWatched() {
   console.log('clik on watched');
-  //   refs.library.innerHTML = createGalleryMarkup(watched); 
+  // refs.library.innerHTML = createGalleryMarkup(watched);
 }
 
 function showQueue() {
-    console.log('clik on queue');
-    //   refs.library.innerHTML = createGalleryMarkup(queue);
+  console.log('clik on queue');
+  //   refs.library.innerHTML = createGalleryMarkup(queue);
 }
