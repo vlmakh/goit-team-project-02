c6ec21b09dd3d112d766d89e2024a702b39a7b8b
Galina <galkozavrik@ukr.net>
2022-12-17 21:06:54 +0300

    додає дві функції, структурує дещо код

--- src/js/local-storage.js
+++ src/js/local-storage.js
@@ -1,7 +1,15 @@
 const KEY_WATCHED = 'watched';
 const KEY_QUEUE = 'queue';
-export const watched = JSON.parse(localStorage.getItem(KEY_WATCHED)) || [];
-export const queue = JSON.parse(localStorage.getItem(KEY_QUEUE)) || [];
+export const watched = getWatchedLocalStoradge() || [];
+export const queue = getQueueLocalStoradge() || [];
+
+export function getWatchedLocalStoradge() {
+  return JSON.parse(localStorage.getItem(KEY_WATCHED));
+}
+
+export function getQueueLocalStoradge() {
+  return JSON.parse(localStorage.getItem(KEY_QUEUE));
+}
 
 export function setWatchedLocalStoradge(arr) {
   localStorage.setItem(KEY_WATCHED, JSON.stringify(arr));
