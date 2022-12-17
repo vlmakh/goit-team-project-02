a57ece24833f57d4fc8efa3d396cd512535910db
Denys Borysiuk <denisborisiuk@gmail.com>
2022-12-17 15:01:29 +0200

    add styles to notice in header

--- src/js/search-by-keyword.js
+++ src/js/search-by-keyword.js
@@ -10,15 +10,25 @@ function onSearchByKeyword(e) {
   let page = 1;
   refs.formWarning.textContent = '';
   if (query === '') {
-    return (refs.formWarning.textContent =
-      'Search query is empty. Enter the correct movie name');
+    setTimeout(() => {
+      refs.formWarning.classList.add('is-hidden');
+    }, 5000);
+    refs.formWarning.classList.remove('is-hidden');
+    refs.formWarning.textContent =
+      'Search query is empty. Enter the correct movie name';
+    return;
   }
 
   getByKeyword(query, page)
     .then(data => {
       if (data.total_results === 0) {
-        return (refs.formWarning.textContent =
-          'Search result not successful. Enter the correct movie name');
+        setTimeout(() => {
+          refs.formWarning.classList.add('is-hidden');
+        }, 5000);
+        refs.formWarning.classList.remove('is-hidden');
+        refs.formWarning.textContent =
+          'Search result not successful. Enter the correct movie name';
+        return;
       }
       console.log(data);
       refs.gallery.innerHTML = createGalleryMarkup(data.results);
