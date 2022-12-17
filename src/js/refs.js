5700246fecdd47fc107fd75aed5f04ed23cc8e07
Eric Ramirez <ramirez1990rer@gmail.com>
2022-12-17 14:07:10 +0200

    added function open_modal

--- src/js/refs.js
+++ src/js/refs.js
@@ -3,6 +3,8 @@ const refs = {
   formWarning: document.querySelector('.header-form__warning'),
   gallery: document.querySelector('.gallery-js'),
   library: document.querySelector('#gallery-lib'),
+  divBackdrop: document.querySelector('.js-backdrop'),
+  closeModalBtn: document.querySelector('.modal__btn-close'),
 };
 
 export default refs;
