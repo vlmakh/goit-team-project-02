da315c0989ba557f89875c68d07926e74b61da50
Vlad Makhnenko <vlmakh@gmail.com>
2022-12-18 14:06:46 +0200

    add library.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-// import './js/rendering-movie-cards';
-// import './js/searchByKeyword';
+import * as renderingMovieCards from './js/rendering-movie-cards';
+import * as searchByKeyword from './js/search-by-keyword';
+import * as openModal from './js/open-and-close-modal';
---
+++ src/library.js
@@ -0,0 +1,2 @@
+import * as renderingFromStorage from './js/render-from-storage';
+import * as openModal from './js/open-and-close-modal';
