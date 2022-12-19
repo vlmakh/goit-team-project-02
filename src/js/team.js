1e6b1c0621c84cbfb8fb31a3f20b5f3b76d7f294
DanaOnopchenko <106885955+DanaOnopchenko@users.noreply.github.com>
2022-12-20 00:43:00 +0200

    upgrade style team and team.js

--- src/js/team.js
+++ src/js/team.js
@@ -1,90 +1,103 @@
 export const teamList = [
-  {
-    name: 'Dana Onopchenko',
-    github: 'https://github.com/DanaOnopchenko',
-    linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
-  },
-
-  {
+ {
     name: 'Vlad Makhnenko',
+    role: 'Team Lead',
     github: 'https://github.com/vlmakh',
     linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
+    photo: 'https://cdn.create.vista.com/api/media/medium/23736039/stock-photo-angry-bee-cartoon-mascot-character?token=1',
   },
-  {
-    name: 'Vlad Makhnenko',
-    github: 'https://github.com/vlmakh',
+   {
+    name: 'Natalie Falach',
+    role: 'Scrum Master',
+    github: 'https://github.com/NatalieFalach',
     linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
+    photo: 'https://cdn.create.vista.com/api/media/small/416832626/stock-photo-cute-cartoon-animal-glasses-illustration',
   },
-  {
-    name: 'Vlad Makhnenko',
-    github: 'https://github.com/vlmakh',
+   {
+    name: 'Galina Danilko',
+    role: 'Developer',
+    github: 'https://github.com/Galinaddd',
     linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
+     photo: 'https://cdn.create.vista.com/api/media/medium/24392501/stock-photo-baby-girl-bee-cartoon-mascot?token=1',
   },
-  {
-    name: 'Vlad Makhnenko',
-    github: 'https://github.com/vlmakh',
+   {
+     name: 'Mykola Saliuk',
+     role: 'Developer',
+    github: 'https://github.com/Mykola0502',
     linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
+    photo: 'https://cdn.create.vista.com/api/media/medium/24350585/stock-photo-happy-bee-cartoon-mascot-character?token=1',
   },
+ 
   {
-    name: 'Vlad Makhnenko',
-    github: 'https://github.com/vlmakh',
+    name: 'Eric Ramirez',
+    role: 'Developer',
+    github: 'https://github.com/Eric-Ramirez-90',
     linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
+    photo: 'https://cdn.create.vista.com/api/media/medium/352615622/stock-photo-cartoon-animal-insect-bee-white?token=1',
   },
-  {
-    name: 'Vlad Makhnenko',
-    github: 'https://github.com/vlmakh',
-    linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
-  },
-  {
-    name: 'Vlad Makhnenko',
-    github: 'https://github.com/vlmakh',
-    linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
-  },
-
-  {
-    name: 'Vlad Makhnenko',
-    github: 'https://github.com/vlmakh',
-    linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
-  },
-  {
-    name: 'Vlad Makhnenko',
-    github: 'https://github.com/vlmakh',
-    linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
-  },
-  {
-    name: 'Vlad Makhnenko',
-    github: 'https://github.com/vlmakh',
-    linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
-  },
-  {
+   {
     name: 'Denis Borysiuk',
+    role: 'Developer',
     github: 'https://github.com/DenysBorysiuk',
     linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
+    photo: 'https://cdn.create.vista.com/api/media/small/7442170/stock-photo-cartoon-bee',
   },
   {
-    name: 'Vlad Makhnenko',
-    role: 'scrum-master',
-    github: 'https://github.com/vlmakh',
+    name: 'Olena Kameneva',
+    role: 'Developer',
+    github: 'https://github.com/Olena0510',
     linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106885955?v=4',
+    photo: 'https://cdn.create.vista.com/api/media/small/346988350/stock-photo-cartoon-animal-insect-bumblebee-white',
   },
   {
-    name: 'Vlad Makhnenko',
-    role: 'team-leader',
-    github: 'https://github.com/vlmakh',
+    name: 'Denis Boyko',
+    role: 'Developer',
+    github: 'https://github.com/Denis070987',
     linkedin: 'https://linkedin.com',
-    photo: 'https://avatars.githubusercontent.com/u/106901780?v=4',
+    photo: 'https://cdn.create.vista.com/api/media/medium/31056783/stock-photo-angry-bee-cartoon-character?token=1',
   },
+  {
+    name: 'Igor Sokolyanskyy',
+    role: 'Developer',
+    github: 'https://github.com/IgorSokolyanskyy',
+    linkedin: 'https://linkedin.com',
+    photo: 'https://cdn.create.vista.com/api/media/medium/30498273/stock-photo-pudgy-bee-cartoon-mascot-character?token=1',
+  },
+  {
+    name: 'Artem Maslii',
+    role: 'Developer',
+    github: 'https://github.com/Tiomasik',
+    linkedin: 'https://linkedin.com',
+     photo: 'https://cdn.create.vista.com/api/media/medium/7276827/stock-photo-happy-bee?token=1',
+  },
+
+  {
+    name: 'Vlad Kolyada',
+    role: 'Developer',
+    github: 'https://github.com/daddy-our-bandera',
+    linkedin: 'https://linkedin.com',
+    photo: 'https://cdn.create.vista.com/api/media/small/109690862/stock-photo-cartoon-bug-bee-isolated',
+  },
+  {
+    name: 'Pavel Zolenko',
+    role: 'Developer',
+    github: 'https://github.com/Pavel-Zolenko',
+    linkedin: 'https://linkedin.com',
+    photo: 'https://cdn.create.vista.com/api/media/small/8427203/stock-photo-cute-bee',
+  },
+  {
+    name: 'Andriy Likhoshva',
+    role: 'Developer',
+    github: 'https://github.com/Andriy-Lik',
+    linkedin: 'https://linkedin.com',
+    photo: 'https://cdn.create.vista.com/api/media/medium/14099387/stock-photo-happy-bee?token=1',
+  },
+   {
+    name: 'Dana Onopchenko',
+    role: 'Developer',
+    github: 'https://github.com/DanaOnopchenko',
+    linkedin: 'https://linkedin.com',
+    photo: 'https://cdn.create.vista.com/api/media/medium/24350621/stock-photo-bumble-bee-cartoon-character-flying?token=1',
+  },
+ 
 ];
