6fcd4a2a14b41664521c80a3bd616a4c3e78c91c
Vlad Makhnenko <vlmakh@gmail.com>
2022-12-19 12:50:08 +0200

    add init

---
+++ src/js/team.js
@@ -0,0 +1,21 @@
+export const teamList = [
+  {
+    name: 'Vlad Makhnenko',
+    github: 'https://github.com/vlmakh',
+    linkedin: 'https://linkedin.com',
+    photo: 'https://avatars.githubusercontent.com/u/106901780?v=4',
+  },
+
+  {
+    name: 'Vlad Makhnenko',
+    github: 'https://github.com/vlmakh',
+    linkedin: 'https://linkedin.com',
+    photo: 'https://avatars.githubusercontent.com/u/106901780?v=4',
+  },
+  {
+    name: 'Vlad Makhnenko',
+    github: 'https://github.com/vlmakh',
+    linkedin: 'https://linkedin.com',
+    photo: 'https://avatars.githubusercontent.com/u/106901780?v=4',
+  },
+];
