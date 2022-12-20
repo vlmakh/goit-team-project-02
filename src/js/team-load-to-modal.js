b72d0d87841b59692786bf408fb6783e3eaadce3
Vlad Makhnenko <vlmakh@gmail.com>
2022-12-20 16:33:06 +0200

    add target blank link

--- src/js/team-load-to-modal.js
+++ src/js/team-load-to-modal.js
@@ -17,10 +17,12 @@ function loadIntoTeamModal(list) {
         </div>
         <div class="member__info">
           <p class="member__name">${member.name}</p>
-          <a class="member__link member__link-git" href="${
-            member.github
-          }">github</a>
-          <a class="member__link" href="${member.linkedin}">linkedin</a>
+          <a class="member__link member__link-git" 
+             href="${member.github}" 
+             target="_blank">github</a>
+          <a class="member__link" 
+          href="${member.linkedin}" 
+          target="_blank">linkedin</a>
           <p class="member__role">${member.role ?? ''}</p>
         </div>
       </li>`;
