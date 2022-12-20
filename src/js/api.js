e6b515d50465b7f6405c6354d3f220d487849c52
Vlad Makhnenko <vlmakh@gmail.com>
2022-12-20 16:15:30 +0200

    del catch

--- src/js/api.js
+++ src/js/api.js
@@ -5,42 +5,30 @@ const API_KEY = '7944ae355bdc42ac579681e106149d6b';
 
 export async function getTrending(page = 1) {
   const url = `${MAIN_URL}/trending/all/day?api_key=${API_KEY}&language=en-US&page=${page}`;
-  return await axios
-    .get(url)
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => console.log(error));
+  return await axios.get(url).then(response => {
+    return response.data;
+  });
 }
 
 export async function getByKeyword(query, page) {
   const url = `${MAIN_URL}/search/movie?api_key=${API_KEY}&query=${query}&language=en-US&page=${page}`;
-  return await axios
-    .get(url)
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => console.log(error));
+  return await axios.get(url).then(response => {
+    return response.data;
+  });
 }
 
 export async function getInfoMovie(movie_id) {
   const url = `${MAIN_URL}/movie/${movie_id}?api_key=${API_KEY}&language=en-US`;
-  return await axios
-    .get(url)
-    .then(response => {
-      return response.data;
-    })
-    .catch(error => console.log(error));
+  return await axios.get(url).then(response => {
+    return response.data;
+  });
 }
 
 export async function getVideos(movie_id) {
   const url = `${MAIN_URL}/movie/${movie_id}/videos?api_key=${API_KEY}&language=en-US`;
-  return await axios
-    .get(url)
-    .then(response => {
-      return response.data.results;
-    })
-    .catch(error => console.log(error));
+  return await axios.get(url).then(response => {
+    return response.data.results;
+  });
 }
 
 export async function getArrayofMovies(array) {
@@ -49,8 +37,7 @@ export async function getArrayofMovies(array) {
       .get(`${MAIN_URL}/movie/${movie_id}?api_key=${API_KEY}&language=en-US`)
       .then(response => {
         return response.data;
-      })
-      .catch(error => console.log(error));
+      });
   });
 
   const resultData = await Promise.all(arrayOfMovies);
